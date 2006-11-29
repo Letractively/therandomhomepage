2e1ee8ea987e2524fa0d692b7ebea870ed5a5b6d
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2006-11-29 20:14:33 +0000

    Fixed JSON parsing issue and added effects

--- src/java/therandomhomepage/common/RandomNewsWidget.java
+++ src/java/therandomhomepage/common/RandomNewsWidget.java
@@ -19,83 +19,83 @@ import java.util.List;
  */
 public class RandomNewsWidget extends RandomWidget {
 
-  private static final String ERROR_MESSAGE = "Error retrieving content from the list of RSS/Atom feed!. Please make sure that the URL is accessible and it is a valid feed.";
-  private String randomFeedScriptURL = null;
-  private static RSSCache cache = new RSSCache();
 
-  public RandomNewsWidget(String header) {
-    super(header);
-  }
+    private String randomFeedScriptURL = null;
 
-  protected void buildFooter() {
-    table.setWidget(3, 0, getAddToGoogleButton());
-  }
 
-  protected void retrieveRandomItem() {
-    randomFeedScriptURL = new URLHelper().getRandomNewsFeedURL();
-    if (cache.getFromCache(randomFeedScriptURL) == null) {
-      if (!HttpRequestUtil.sendAsyncGetRequest(randomFeedScriptURL, new RSSItemResponseHandler(this))) {
-        showError();
-      }
-    } else {
-      List itemList = cache.getFromCache(randomFeedScriptURL);
-      RSSItem rssItem = (RSSItem) Randomizer.getRandomItem(itemList);
-      displayRandomItem(rssItem);
-    }
-  }
-
-  private void showError() {
-    Label errorLabel = new Label(RandomNewsWidget.ERROR_MESSAGE);
-      //noinspection GWTStyleCheck
-      errorLabel.setStyleName("randomWidgetHeader-errorMessage");
-    table.setWidget(1, 0, errorLabel);
-  }
-
-  private class RSSItemResponseHandler implements ResponseTextHandler {
-
-    private String feedURL;
-    private RandomNewsWidget widget;
-
-    public RSSItemResponseHandler(RandomNewsWidget widget) {
-      this.widget = widget;
+    public RandomNewsWidget(String header) {
+        super(header);
     }
 
-    public RSSItemResponseHandler(RandomNewsWidget widget, String feedURL) {
-      this(widget);
-      this.feedURL = feedURL;
+    protected void buildFooter() {
+        table.setWidget(3, 0, getAddToGoogleButton());
     }
 
-    public void onCompletion(String responseText) {
-      System.out.println("responseText = " + responseText);  
-      if (!HttpRequestUtil.isErrorResponse(responseText)) {
-        List rssItems = JSON2RSSParser.parse(responseText);
-        if (feedURL == null) {
-          widget.addToCache(randomFeedScriptURL, rssItems);
+    protected void retrieveRandomItem() {
+        randomFeedScriptURL = new URLHelper().getRandomNewsFeedURL();
+        if (cache.getFromCache(randomFeedScriptURL) == null) {
+            if (!HttpRequestUtil.sendAsyncGetRequest(randomFeedScriptURL, new RSSItemResponseHandler(this))) {
+                showError();
+            }
         } else {
-          widget.addToCache(feedURL, rssItems);
+            List itemList = cache.getFromCache(randomFeedScriptURL);
+            RSSItem rssItem = (RSSItem) Randomizer.getRandomItem(itemList);
+            displayRandomItem(rssItem);
         }
-        RSSItem randomItem = (RSSItem) Randomizer.getRandomItem(rssItems);
-        displayRandomItem(randomItem);
-      }
     }
-  }
 
-  private void displayRandomItem(RSSItem randomItem) {
-    if (randomItem != null) {
-      RSSItemLink itemLink = new RSSItemLink(randomItem);
-      table.setWidget(1, 0, itemLink);
-      table.getFlexCellFormatter().setColSpan(1, 0, 2);
-      table.getFlexCellFormatter().setVerticalAlignment(0, 1, HasVerticalAlignment.ALIGN_TOP);
-      table.setWidget(2, 0, new HTML(randomItem.getDesc()));
-      table.getFlexCellFormatter().setColSpan(2, 0, 2);
+    private void showError() {
+        Label errorLabel = new Label(ERROR_MESSAGE);
+        //noinspection GWTStyleCheck
+        errorLabel.setStyleName("randomWidgetHeader-errorMessage");
+        table.setWidget(1, 0, errorLabel);
     }
-  }
 
-  private void addToCache(String feedURL, List rssItems) {
-    cache.addToCache(feedURL, rssItems);
-  }
+    private class RSSItemResponseHandler implements ResponseTextHandler {
 
-  private HTML getAddToGoogleButton() {
-    return new HTML("<a href=\"http://fusion.google.com/add?moduleurl=http%3A//www.therandomhomepage.com/google/gadget/RandomFeedModule.xml\"><img src=\"http://buttons.googlesyndication.com/fusion/add.gif\" width=\"104\" height=\"17\" border=\"0\" alt=\"Add to Google\"></a>");
-  }
+        private String feedURL;
+        private RandomNewsWidget widget;
+
+        public RSSItemResponseHandler(RandomNewsWidget widget) {
+            this.widget = widget;
+        }
+
+        public RSSItemResponseHandler(RandomNewsWidget widget, String feedURL) {
+            this(widget);
+            this.feedURL = feedURL;
+        }
+
+        public void onCompletion(String responseText) {
+            System.out.println("responseText = " + responseText);
+            if (!HttpRequestUtil.isErrorResponse(responseText)) {
+                List rssItems = JSON2RSSParser.parse(responseText);
+                if (feedURL == null) {
+                    widget.addToCache(randomFeedScriptURL, rssItems);
+                } else {
+                    widget.addToCache(feedURL, rssItems);
+                }
+                RSSItem randomItem = (RSSItem) Randomizer.getRandomItem(rssItems);
+                displayRandomItem(randomItem);
+            }
+        }
+    }
+
+    private void displayRandomItem(RSSItem randomItem) {
+        if (randomItem != null) {
+            RSSItemLink itemLink = new RSSItemLink(randomItem);
+            table.setWidget(1, 0, itemLink);
+            table.getFlexCellFormatter().setColSpan(1, 0, 2);
+            table.getFlexCellFormatter().setVerticalAlignment(0, 1, HasVerticalAlignment.ALIGN_TOP);
+            table.setWidget(2, 0, new HTML(randomItem.getDesc()));
+            table.getFlexCellFormatter().setColSpan(2, 0, 2);
+        }
+    }
+
+    private void addToCache(String feedURL, List rssItems) {
+
+    }
+
+    private HTML getAddToGoogleButton() {
+        return new HTML("<a href=\"http://fusion.google.com/add?moduleurl=http%3A//www.therandomhomepage.com/google/gadget/RandomFeedModule.xml\"><img src=\"http://buttons.googlesyndication.com/fusion/add.gif\" width=\"104\" height=\"17\" border=\"0\" alt=\"Add to Google\"></a>");
+    }
 }
---
+++ src/java/therandomhomepage/randomflickrclient/EffectsHelper.java
@@ -0,0 +1,93 @@
+package therandomhomepage.randomflickrclient;
+
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.ui.Widget;
+import org.gwtwidgets.client.wrap.Callback;
+import org.gwtwidgets.client.wrap.Effect;
+import org.gwtwidgets.client.wrap.EffectOption;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Siddique Hameed
+ * Date: Nov 29, 2006
+ * Time: 2:31:48 PM
+ */
+public class EffectsHelper {
+
+    public static final int RANDOM = -1;
+    public static final int FADE = 0;
+    public static final int BLINDOWN = 1;
+    public static final int BLINDUP = 2;
+    public static final int SWITCHOFF = 3;
+    public static final int HIGHLIGHT = 4;
+    public static final int DROPOUT = 5;
+    public static final int SHAKE = 6;
+    public static final int PULSATE = 7;
+    public static final int SQUISH = 8;
+    public static final int FOLD = 9;
+    public static final int GROW = 10;
+    public static final int SHRINK = 11;
+    public static final int PUFF = 12;
+    public static final int SLIDEDOWN = 13;
+    public static final int SLIDEUP = 14;
+    private static final String AFTER_FINISH_EFFECT_OPTION = "afterFinish";
+
+
+    public static void applyEffects(Widget widget, int effectConst) {
+        switch (effectConst) {
+            case RANDOM:
+                int i = Random.nextInt(SLIDEUP);
+                applyEffects(widget, i);
+                break;
+            case FADE:
+                Effect.fade(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case BLINDOWN:
+                Effect.blindDown(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case BLINDUP:
+                Effect.blindUp(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case SWITCHOFF:
+                Effect.switchOff(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case HIGHLIGHT:
+                Effect.highlight(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case DROPOUT:
+                Effect.dropOut(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case SHAKE:
+                Effect.shake(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case PULSATE:
+                Effect.pulsate(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case SQUISH:
+                Effect.squish(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case PUFF:
+                Effect.puff(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case SLIDEDOWN:
+                Effect.slideDown(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+            case SLIDEUP:
+                Effect.slideUp(widget, new EffectOption[]{new EffectOption(AFTER_FINISH_EFFECT_OPTION, new EffectsCallback(widget))});
+                break;
+        }
+    }
+
+
+    private static class EffectsCallback implements Callback {
+        private Widget widget;
+
+        EffectsCallback(Widget widget) {
+            this.widget = widget;
+        }
+
+        public void execute() {
+            Effect.appear(widget);
+        }
+    }
+}
