ba85c9edff8018dcbc610a0e870eba1ce2b389c4
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2006-11-30 16:44:29 +0000

    Created common module

--- src/java/therandomhomepage/common/RSS2XMLDocumentParser.java
+++ src/java/therandomhomepage/common/RSS2XMLDocumentParser.java
@@ -1,12 +1,11 @@
 package therandomhomepage.common;
 
-import com.google.gwt.xml.client.XMLParser;
 import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.XMLParser;
 import com.google.gwt.xml.client.NodeList;
 import com.google.gwt.xml.client.Node;
 
 import java.util.List;
-import java.util.Iterator;
 import java.util.ArrayList;
 
 import therandomhomepage.common.rss.RSSItem;
@@ -35,16 +34,13 @@ public class RSS2XMLDocumentParser {
 
             for (int j = 0; j < childNodes.getLength(); j++) {
                 Node childElement = childNodes.item(j);
-                if (childElement.getNodeName().equals("title")){
+                if (childElement.getNodeName().equals("title")) {
                     rssItem.setTitle(childElement.getNodeValue());
-                }
-                else if (childElement.getNodeName().equals("description")){
+                } else if (childElement.getNodeName().equals("description")) {
                     rssItem.setDesc(childElement.getNodeValue());
-                }
-                else if (childElement.getNodeName().equals("guid")){
+                } else if (childElement.getNodeName().equals("guid")) {
                     rssItem.setGuid(childElement.getNodeValue());
-                }
-                else if (childElement.getNodeName().equals("media:content")){
+                } else if (childElement.getNodeName().equals("media:content")) {
                     //TODO: parse media content
                 }
 
