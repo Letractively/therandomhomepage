83214a4ba0870fd1088a824bbc7c8cc5c065d130
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2006-12-15 21:46:01 +0000

    change in stylesheet

--- widgets/Lightbox Image/java/therandomhomepage/widgets/client/LightboxImage.java
+++ widgets/Lightbox Image/java/therandomhomepage/widgets/client/LightboxImage.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2006 Siddique Hameed <siddii AT gmail.com> - http://www.therandomhomepage.com
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package therandomhomepage.widgets.client;
 
 import com.google.gwt.user.client.DOM;
@@ -8,10 +25,11 @@ import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Siddique Hameed
- * Date: Dec 14, 2006
- * Time: 11:13:59 AM
+ * LightboxImage widget, a GWT wrapper for Lightbox JS by Lokesh Dhakar.
+ * @see <a target="_new" href="http://www.huddletogether.com/projects/lightbox2/">http://www.huddletogether.com/projects/lightbox2/</a>
+ *
+ * @author Siddique Hameed
+ * @version 1.0
  */
 public class LightboxImage extends Widget {
 
