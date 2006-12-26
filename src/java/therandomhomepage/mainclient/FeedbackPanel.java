4e4e56d6a6c89abbc1d2759b641de522fb360591
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2006-12-26 19:23:25 +0000

    WIP - Feedback panel

--- src/java/therandomhomepage/mainclient/FeedbackPanel.java
+++ src/java/therandomhomepage/mainclient/FeedbackPanel.java
@@ -15,87 +15,91 @@ public class FeedbackPanel extends Composite {
 
     public FeedbackPanel() {
 
-        final FormPanel form = new FormPanel();
+        NamedFrame mailFrame = new NamedFrame("mailFrame");
+        mailFrame.setHeight("0px");
+        mailFrame.setWidth("0px");
+
+        final FormPanel form = new FormPanel(mailFrame);
         form.setAction("/php/sendMail.php");
 
+        form.setEncoding(FormPanel.ENCODING_URLENCODED);
         form.setMethod(FormPanel.METHOD_POST);
 
         VerticalPanel panel = new VerticalPanel();
         panel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
         panel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
 
-        form.setWidget(panel);
+
+        panel.add(form);
+
+        panel.add(mailFrame);
 
         initWidget(panel);
 
         FlexTable table = new FlexTable();
-        table.setStyleName("divBlock");
+        table.setStyleName("FeedbackForm");
 
-        table.setWidget(0, 0, new Label("Name: "));
+        table.setWidget(0, 0, new HTML("<h3>Feedback</h3>"));
+
+        table.setWidget(1, 0, new Label("Name: "));
         TextBox txtName = new TextBox();
         txtName.setName("txtName");
-        table.setWidget(0, 1, txtName);
+        table.setWidget(1, 1, txtName);
 
-        table.setWidget(1, 0, new Label("Email: "));
+        table.setWidget(2, 0, new Label("Email: "));
         TextBox txtEmail = new TextBox();
         txtEmail.setName("txtEmail");
-        table.setWidget(1, 1, txtEmail);
+        table.setWidget(2, 1, txtEmail);
 
-        table.setWidget(2, 0, new Label("Subject: "));
+        table.setWidget(3, 0, new Label("Subject: "));
 
         ListBox subject = new ListBox();
         subject.setName("selSubject");
-        subject.addItem("General");
-        subject.addItem("Comments");
-        subject.addItem("Suggestion");
-        subject.addItem("Bug report");
-        subject.addItem("Others");
+        subject.addItem("General", "General");
+        subject.addItem("Comments", "Comments");
+        subject.addItem("Suggestion", "Suggestion");
+        subject.addItem("Bug report", "Bug report");
+        subject.addItem("Others", "Others");
 
-        table.setWidget(2, 1, subject);
+        table.setWidget(3, 1, subject);
 
-        table.setWidget(3, 0, new Label("Message: "));
-
+        table.setWidget(4, 0, new Label("Message: "));
 
         txtMessage = new TextArea();
         txtMessage.setName("txtMessage");
-        txtMessage.setCharacterWidth(100);
-        txtMessage.setVisibleLines(10);
-        table.setWidget(3, 1, txtMessage);
+        txtMessage.setCharacterWidth(50);
+        txtMessage.setVisibleLines(8);
+        table.setWidget(4, 1, txtMessage);
 
-
-
-        // Add a 'submit' button.
         Button btnSubmit = new Button("Submit", new ClickListener() {
             public void onClick(Widget sender) {
-                Window.alert("Submitting form");
                 form.submit();
             }
         });
 
-        table.getFlexCellFormatter().setColSpan(4, 0, 2);
-        table.setWidget(4,0,btnSubmit);
+//        table.getFlexCellFormatter().setColSpan(5, 0, 2);
+        table.setWidget(5, 0, btnSubmit);
 
-        panel.add(table);
+        form.add(table);
 
         // Add an event handler to the form.
         form.addFormHandler(new FormHandler() {
             public void onSubmitComplete(FormSubmitCompleteEvent event) {
-                // When the form submission is successfully completed, this event is
-                // fired. Assuming the service returned a response of type text/plain,
-                // we can get the result text here (see the FormPanel documentation for
-                // further explanation).
-                Window.alert(event.getResults());
+                // do nothing
             }
 
             public void onSubmit(FormSubmitEvent event) {
                 // This event is fired just before the form is submitted. We can take
                 // this opportunity to perform validation.
+
+
                 if (txtMessage.getText().length() == 0) {
-                    Window.alert("The text box must not be empty");
+                    Window.alert("Please provide ");
                     event.setCancelled(true);
                 }
             }
         });
+
 
     }
 }
