cf2db3121fac4317ead35d68fde5ef3e01bf68b6
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2006-12-29 14:05:51 +0000

    Debugging feedback mail sending.

--- src/java/therandomhomepage/mainclient/FeedbackPanel.java
+++ src/java/therandomhomepage/mainclient/FeedbackPanel.java
@@ -17,79 +17,141 @@ public class FeedbackPanel extends Composite {
     private FlexTable table;
 
 
+//    public FeedbackPanel() {
+//
+//        NamedFrame mailFrame = new NamedFrame("mailFrame");
+//        mailFrame.setVisible(false);
+//
+//        final FormPanel form = new FormPanel(mailFrame);
+//        form.setAction("/php/sendMail.php");
+//
+//        form.setMethod(FormPanel.METHOD_GET);
+//        form.setEncoding(FormPanel.ENCODING_URLENCODED);
+//
+//        VerticalPanel panel = new VerticalPanel();
+//        panel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
+//        panel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
+//
+//
+//        panel.add(form);
+//
+//        panel.add(mailFrame);
+//
+//        initWidget(panel);
+//
+//        table = new FlexTable();
+//        table.addStyleName("divBlock");
+//        table.addStyleName("feedbackForm");
+//
+//        table.setWidget(0, 0, new HTML("<h3>Feedback</h3>"));
+//
+//        table.setWidget(1, 0, new Label("Name: "));
+//        table.getFlexCellFormatter().setVerticalAlignment(1, 0, HasVerticalAlignment.ALIGN_TOP);
+//
+//        TextBox txtName = new TextBox();
+//        txtName.setName("txtName");
+//        table.setWidget(1, 1, txtName);
+//
+//        table.setWidget(2, 0, new Label("Email: "));
+//        table.getFlexCellFormatter().setVerticalAlignment(2, 0, HasVerticalAlignment.ALIGN_TOP);
+//
+//        txtEmail = new TextBox();
+//        txtEmail.setName("txtEmail");
+//        table.setWidget(2, 1, txtEmail);
+//
+//        table.setWidget(3, 0, new Label("Subject: "));
+//        table.getFlexCellFormatter().setVerticalAlignment(3, 0, HasVerticalAlignment.ALIGN_TOP);
+//
+//        ListBox subject = new ListBox();
+//        subject.setName("selSubject");
+//        subject.addItem("General", "General");
+//        subject.addItem("Comments", "Comments");
+//        subject.addItem("Suggestion", "Suggestion");
+//        subject.addItem("Bug report", "Bug report");
+//        subject.addItem("Others", "Others");
+//
+//        table.setWidget(3, 1, subject);
+//
+//        table.setWidget(4, 0, new Label("Message: "));
+//        table.getFlexCellFormatter().setVerticalAlignment(4, 0, HasVerticalAlignment.ALIGN_TOP);
+//
+//        txtMessage = new TextArea();
+//        txtMessage.setName("txtMessage");
+//        txtMessage.setCharacterWidth(50);
+//        txtMessage.setVisibleLines(8);
+//        table.setWidget(4, 1, txtMessage);
+//
+//        Button btnSubmit = new Button("Submit", new ClickListener() {
+//            public void onClick(Widget sender) {
+//                form.submit();
+//            }
+//        });
+//
+//        table.setWidget(5, 0, btnSubmit);
+//
+//        form.add(table);
+//
+//        form.addFormHandler(new FormHandler() {
+//            public void onSubmitComplete(FormSubmitCompleteEvent event) {
+//                Window.alert(event.getResults());
+//            }
+//
+//            public void onSubmit(FormSubmitEvent event) {
+//                if (txtEmail.getText().trim().length() == 0) {
+//                    Window.alert("Please provide your email address !");
+//                    event.setCancelled(true);
+//                    txtEmail.setFocus(true);
+//                } else if (txtMessage.getText().trim().length() == 0) {
+//                    Window.alert("Please provide feedback !");
+//                    event.setCancelled(true);
+//                    txtMessage.setFocus(true);
+//                } else {
+//                    table.clear();
+//                    HTML thanksHTML = new HTML("<h4>We really appreciate your feedback. Thanks a lot !</h4");
+//                    table.setWidget(0, 0, thanksHTML);
+//                    DOM.setAttribute(table.getWidget(0, 0).getElement(), "align", "center");
+//                    DOM.setAttribute(table.getCellFormatter().getElement(0, 0), "width", "100%");
+//                }
+//            }
+//        });
+//    }
+
     public FeedbackPanel() {
+
+        VerticalPanel fp = new VerticalPanel();
 
         NamedFrame mailFrame = new NamedFrame("mailFrame");
         mailFrame.setVisible(false);
-
-        final FormPanel form = new FormPanel(mailFrame);
+        final FormPanel form=new FormPanel(mailFrame);
+        form.setEncoding(FormPanel.ENCODING_URLENCODED);
         form.setAction("/php/sendMail.php");
 
-        form.setMethod(FormPanel.METHOD_GET);
-        form.setEncoding(FormPanel.ENCODING_URLENCODED);
-
-        VerticalPanel panel = new VerticalPanel();
-        panel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-        panel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
-
-
-        panel.add(form);
-
-        panel.add(mailFrame);
-
-        initWidget(panel);
-
-        table = new FlexTable();
-        table.addStyleName("divBlock");
-        table.addStyleName("feedbackForm");
-
-        table.setWidget(0, 0, new HTML("<h3>Feedback</h3>"));
-
-        table.setWidget(1, 0, new Label("Name: "));
-        table.getFlexCellFormatter().setVerticalAlignment(1, 0, HasVerticalAlignment.ALIGN_TOP);
-
-        TextBox txtName = new TextBox();
+        TextBox txtName=new TextBox();
         txtName.setName("txtName");
-        table.setWidget(1, 1, txtName);
-
-        table.setWidget(2, 0, new Label("Email: "));
-        table.getFlexCellFormatter().setVerticalAlignment(2, 0, HasVerticalAlignment.ALIGN_TOP);
-
-        txtEmail = new TextBox();
+        txtEmail=new TextBox();
         txtEmail.setName("txtEmail");
-        table.setWidget(2, 1, txtEmail);
-
-        table.setWidget(3, 0, new Label("Subject: "));
-        table.getFlexCellFormatter().setVerticalAlignment(3, 0, HasVerticalAlignment.ALIGN_TOP);
-
-        ListBox subject = new ListBox();
-        subject.setName("selSubject");
-        subject.addItem("General", "General");
-        subject.addItem("Comments", "Comments");
-        subject.addItem("Suggestion", "Suggestion");
-        subject.addItem("Bug report", "Bug report");
-        subject.addItem("Others", "Others");
-
-        table.setWidget(3, 1, subject);
-
-        table.setWidget(4, 0, new Label("Message: "));
-        table.getFlexCellFormatter().setVerticalAlignment(4, 0, HasVerticalAlignment.ALIGN_TOP);
+        TextBox selSubject=new TextBox();
+        selSubject.setName("selSubject");
 
         txtMessage = new TextArea();
         txtMessage.setName("txtMessage");
-        txtMessage.setCharacterWidth(50);
-        txtMessage.setVisibleLines(8);
-        table.setWidget(4, 1, txtMessage);
 
-        Button btnSubmit = new Button("Submit", new ClickListener() {
+        Button b=new Button("Submit");
+        b.addClickListener(new ClickListener() {
             public void onClick(Widget sender) {
                 form.submit();
             }
         });
 
-        table.setWidget(5, 0, btnSubmit);
 
-        form.add(table);
+        fp.add(txtName);
+        fp.add(txtEmail);
+        fp.add(selSubject);
+        fp.add(txtMessage);
+        fp.add(b);
+
+        fp.add(mailFrame);
+        form.add(fp);
 
         form.addFormHandler(new FormHandler() {
             public void onSubmitComplete(FormSubmitCompleteEvent event) {
@@ -106,15 +168,19 @@ public class FeedbackPanel extends Composite {
                     event.setCancelled(true);
                     txtMessage.setFocus(true);
                 } else {
-                    table.clear();
-                    HTML thanksHTML = new HTML("<h4>We really appreciate your feedback. Thanks a lot !</h4");
-                    table.setWidget(0, 0, thanksHTML);
-                    DOM.setAttribute(table.getWidget(0, 0).getElement(), "align", "center");
-                    DOM.setAttribute(table.getCellFormatter().getElement(0, 0), "width", "100%");
+                    Window.alert("Message sent successfully !");
+//                    table.clear();
+//                    HTML thanksHTML = new HTML("<h4>We really appreciate your feedback. Thanks a lot !</h4");
+//                    table.setWidget(0, 0, thanksHTML);
+//                    DOM.setAttribute(table.getWidget(0, 0).getElement(), "align", "center");
+//                    DOM.setAttribute(table.getCellFormatter().getElement(0, 0), "width", "100%");
                 }
             }
         });
 
+
+
+        initWidget(form);
 
     }
 }
