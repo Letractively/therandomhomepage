99be7940d53a5cd20dafb934cc33fcf2353a086b
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2007-01-04 18:26:06 +0000

    Removing lightbox.css as it is part of LightboxImage widget

---
+++ widgets/src/java/therandomhomepage/widgets/client/MediaPlayer.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2006 Siddique Hameed <siddii AT gmail.com> - http://www.TheRandomHomepage.com
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package therandomhomepage.widgets.client;
+
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Siddique Hameed
+ * Date: Jan 4, 2007
+ * Time: 11:49:10 AM
+ */
+public class MediaPlayer extends Widget {
+    //TODO: SWF File
+}
