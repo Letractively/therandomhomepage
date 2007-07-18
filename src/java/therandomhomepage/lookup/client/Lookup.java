bdb4cfb3f05bc9258cee0834f19bc0830c4b2fa0
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2007-07-18 20:22:01 +0000

    Working on lookup module

--- src/java/therandomhomepage/lookup/client/Lookup.java
+++ src/java/therandomhomepage/lookup/client/Lookup.java
@@ -4,6 +4,13 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.HTTPRequest;
+import com.google.gwt.user.client.ResponseTextHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONArray;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -14,13 +21,47 @@ public class Lookup implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
+        final TextBox txtBox = new TextBox();
+
         final Button button = new Button("Click me");
         final Label label = new Label();
 
         button.addClickListener(new ClickListener() {
             public void onClick(Widget sender) {
                 if (label.getText().equals("")) {
-                    LookupService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
+
+                    String url = "http://spreadsheets.google.com/feeds/cells/pSYncC12rB8CTEnrWbHreeA/od6/public/values?alt=json";
+
+                    HTTPRequest.asyncGet(url,new ResponseTextHandler(){
+
+                        public void onCompletion(String responseText) {
+                            Window.alert("responseText = "+responseText);
+                            JSONValue jsonValue = JSONParser.parse(responseText);
+                            JSONObject jsonObject = jsonValue.isObject();
+
+                            if (jsonObject != null){
+                                JSONValue feedValue = jsonObject.get("feed");
+                                if (feedValue != null && feedValue.isObject() != null){
+                                    JSONObject feedObject = feedValue.isObject();
+                                        JSONValue entryObject = feedObject.get("entry");
+                                        if (entryObject != null && entryObject.isArray() != null){
+                                            JSONArray entryArray = entryObject.isArray();
+                                            for(int i=0; i < entryArray.size(); i++){
+                                                JSONValue entryValue = entryArray.get(i);
+                                                if (entryValue != null && entryValue.isObject() != null){
+                                                    JSONValue cell = entryValue.isObject().get("gs$cell");
+                                                    System.out.println("cell.toString() = " + cell.toString());
+                                                }
+                                            }
+                                        }
+
+                                }
+                            }
+
+                        }
+                    });
+
+//                    LookupService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
                 } else
                     label.setText("");
             }
@@ -31,6 +72,7 @@ public class Lookup implements EntryPoint {
         // to hard-code IDs.  Instead, you could, for example, search for all
         // elements with a particular CSS class and replace them with widgets.
         //
+        RootPanel.get("inputSlot").add(txtBox);
         RootPanel.get("slot1").add(button);
         RootPanel.get("slot2").add(label);
     }
