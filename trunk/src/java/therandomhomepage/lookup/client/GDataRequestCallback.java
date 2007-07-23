f4b7b52672b4d2524cef518a625f1134d6a2f7ed
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2007-07-23 21:12:36 +0000

    working on lookup gadget

---
+++ trunk/src/java/therandomhomepage/lookup/client/GDataRequestCallback.java
@@ -0,0 +1,16 @@
+package therandomhomepage.lookup.client;
+
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.Request;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Siddique Hameed
+ * Date: Jul 23, 2007
+ * Time: 5:03:45 PM
+ */
+public abstract class GDataRequestCallback implements RequestCallback {
+    public void onError(Request request, Throwable exception) {
+        exception.printStackTrace();
+    }
+}
---
+++ trunk/src/java/therandomhomepage/lookup/client/LoginCallback.java
@@ -0,0 +1,12 @@
+package therandomhomepage.lookup.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Siddique Hameed
+ * Date: Jul 23, 2007
+ * Time: 1:43:11 PM
+ */
+public interface LoginCallback {
+    public void onSuccess();
+    public void onFailure();
+}
