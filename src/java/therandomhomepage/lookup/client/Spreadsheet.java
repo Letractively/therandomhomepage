d56191b4c44449caab15ee5d59157ef2087d98a9
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2007-07-23 15:12:03 +0000

    working on lookup gadget

---
+++ src/java/therandomhomepage/lookup/client/Spreadsheet.java
@@ -0,0 +1,15 @@
+package therandomhomepage.lookup.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Siddique Hameed
+ * Date: Jul 23, 2007
+ * Time: 10:31:35 AM
+ */
+public class Spreadsheet {
+
+    public void showSpreadsheetMetafeed(){
+        
+    }
+    
+}
