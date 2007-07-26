788fcb880ae054bb34a3b841122cbf20a9694127
siddii <siddii@75094fa0-391b-0410-9882-0575bf9cc8d9>
2007-07-26 19:07:29 +0000

    working on lookup gadget

--- trunk/src/java/therandomhomepage/lookup/client/GDataRequest.java
+++ trunk/src/java/therandomhomepage/lookup/client/GDataRequest.java
@@ -11,59 +11,33 @@ import com.google.gwt.http.client.RequestException;
  * Date: Jul 23, 2007
  * Time: 2:09:48 PM
  */
-public class GDataRequest {
+public class GDataRequest extends RequestBuilder {
 
-    private String url;
-    private RequestBuilder requestBuilder = null;
-
-
-    public GDataRequest(String url) {
-        this.url = url;
+    public GDataRequest(String httpMethod, String url) {
+        super(httpMethod, url);
+        setHeader("Authorization", "GoogleLogin auth=" + Authentication.getToken());
     }
 
+    public GDataRequest(String url) {
+        this("GET", url);
+    }
 
     public Request sendGetRequest(RequestCallback callback) {
         try {
-            initRequestBuilder(RequestBuilder.GET);
-            return requestBuilder.sendRequest(null, callback);
+            return sendRequest(null, callback);
         } catch (RequestException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public Request sendPostRequest(String postData,RequestCallback callback) {
+    public Request sendPostRequest(String postData, RequestCallback callback) {
         try {
-            initRequestBuilder(RequestBuilder.POST);
-            requestBuilder.setHeader("Content-Type","application/atom+xml");
-            return requestBuilder.sendRequest(postData, callback);
+            setHeader("Content-Type", "application/atom+xml");
+            sendRequest(postData, callback);
         } catch (RequestException e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public Request sendPutRequest(String postData,RequestCallback callback) {
-        try {
-            requestBuilder = new MyRequestBuilder("PUT",url);
-            requestBuilder.setHeader("Authorization", "GoogleLogin auth=" + Authentication.getToken());
-            requestBuilder.setHeader("Content-Type","application/atom+xml");
-            return requestBuilder.sendRequest(postData, callback);
-        } catch (RequestException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void initRequestBuilder(RequestBuilder.Method requestMethod) {
-        requestBuilder = new RequestBuilder(requestMethod,url);
-        System.out.println("Authentication.getToken() = " + Authentication.getToken());
-        requestBuilder.setHeader("Authorization", "GoogleLogin auth=" + Authentication.getToken());
-    }
-
-    private class MyRequestBuilder extends RequestBuilder{
-        protected MyRequestBuilder(String httpMethod, String url) {
-            super(httpMethod, url);
-        }
     }
 }
