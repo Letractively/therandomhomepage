1af0ebe6ecb606ec6fede71cf0b71e1eca521e93
siddii@gmail.com <siddii@gmail.com@75094fa0-391b-0410-9882-0575bf9cc8d9>
2012-02-20 18:55:44 +0000

    update

--- trunk/google/gadgets/NewsReader/storage.js
+++ trunk/google/gadgets/NewsReader/storage.js
@@ -3,9 +3,11 @@
 
 */
 
+var DEFAULT_CACHE_TIME = 
+
 function Storage (name,cacheTime) {
     this.name = name;
-    this.cacheTime = "red";
+    this.cacheTime = !cacheTime ? 
     this.addValue= function(key, value) {
         
     };
