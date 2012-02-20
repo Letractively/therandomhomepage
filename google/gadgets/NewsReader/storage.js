2283dfd759b827ec781371be420faf7972b1edca
siddii <siddii@gmail.com>
2012-02-20 18:52:13 +0000

    first revision

---
+++ google/gadgets/NewsReader/storage.js
@@ -0,0 +1,16 @@
+/*
+
+
+*/
+
+function Storage (name,cacheTime) {
+    this.name = name;
+    this.cacheTime = "red";
+    this.addValue= function(key, value) {
+        
+    };
+    this.getValue = function(key) {
+    };      
+} 
+
+
